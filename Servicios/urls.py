62d762b16819e88c801a4f5adc8d8bea452c3ea7
Louie壮真UwUr_柔 <126754704+LoiueFragosoUwUr@users.noreply.github.com>
2023-05-31 23:18:48 -0600

    Delete Servicios directory

--- Servicios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Servicios import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', views.servicios, name="Servicios"),
-]
